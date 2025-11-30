934013f35a924dd7c7625f9252ad40a70238c327
mostafa <most3f3.1112006@gmail.com>
2025-11-30 17:52:26 +0200

    Added features .

--- src/Main.cpp
+++ src/Main.cpp
@@ -44,73 +44,93 @@ void printMatrix(const vector<vector<double>>& mat) {
 }
 
 
-int main() {
-    Menu menu;
-    menu.Display();
-    int op = menu.getOption();
+int main()
+{
+    while (true)   // LOOP start
+    {
+        Menu menu;
+        menu.Display();
+        int op = menu.getOption();
 
-    Matrix A, B;
-    bool needSecond = (op == 1 || op == 2 || op == 4);
-    A = inputMatrixFromUser("Matrix A");
-    if (needSecond)
-        B = inputMatrixFromUser("MAtrix B");
-
-    try {
-        switch (op) {
-
-        case 1:
-            printMatrix(A.addition(B).getMAtrix());
-            break;
-
-        case 2:
-            printMatrix(A.subtraction(B).getMAtrix());
-            break;
-
-        case 3: {
-            double scalar;
-            cout << "Enter scalar: ";
-            cin >> scalar;
-            printMatrix(A.scalarMultiplication(scalar).getMAtrix());
+        if (op == 0) {
+            cout << GREEN << "Exiting program...\n" << RESET;
             break;
         }
 
-        case 4:
-            printMatrix(A.multiplication(B).getMAtrix());
-            break;
+        Matrix A, B;
+        bool needSecond = (op == 1 || op == 2 || op == 4);
 
-        case 5:
-            cout << "Trace = " << A.trace() << "\n";
-            break;
+        A = inputMatrixFromUser("A");
+        if (needSecond)
+            B = inputMatrixFromUser("B");
 
-        case 6:
-            printMatrix(A.transpose().getMAtrix());
-            break;
+        try {
+            switch (op) {
 
-        case 7:
-            cout << "Determinant = " << A.determinant() << "\n";
-            break;
+            case 1:
+                printMatrix(A.addition(B).getMAtrix());
+                break;
 
-        case 8: {
-            int p;
-            cout << "Enter power: ";
-            cin >> p;
-            printMatrix(A.Matrix_power(p).getMAtrix());
-            break;
+            case 2:
+                printMatrix(A.subtraction(B).getMAtrix());
+                break;
+
+            case 3: {
+                double scalar;
+                cout << "Enter scalar: ";
+                cin >> scalar;
+                printMatrix(A.scalarMultiplication(scalar).getMAtrix());
+                break;
+            }
+
+            case 4:
+                printMatrix(A.multiplication(B).getMAtrix());
+                break;
+
+            case 5:
+                cout << "Trace = " << A.trace() << "\n";
+                break;
+
+            case 6:
+                printMatrix(A.transpose().getMAtrix());
+                break;
+
+            case 7:
+                cout << "Determinant = " << A.determinant() << "\n";
+                break;
+
+            case 8: {
+                int p;
+                cout << "Enter power: ";
+                cin >> p;
+                printMatrix(A.Matrix_power(p).getMAtrix());
+                break;
+            }
+
+            case 9: {
+                auto [L, U] = A.LU();
+                cout << BOLD_GREEN << "\nL Matrix:\n" << RESET;
+                printMatrix(L.getMAtrix());
+                cout << BOLD_GREEN << "\nU Matrix:\n" << RESET;
+                printMatrix(U.getMAtrix());
+                break;
+            }
+
+            default:
+                cout << RED << "Invalid Option\n" << RESET;
+            }
+
+        }
+        catch (const exception& e) {
+            cout << RED << "Error: " << e.what() << "\n" << RESET;
         }
 
-        case 9:
-            printMatrix(A.LU_Factorization().getMAtrix());
-            break;
-
-        default:
-            cout << "Invalid Option\n";
-        }
-
-    }
-    catch (const exception& e) {
-        cout << "Error: " << e.what() << "\n";
+        cout << BOLD_YELLOW << "\nPress ENTER to continue..." << RESET;
+        cin.ignore();
+        cin.get();     
     }
 
     return 0;
 }
+
 
