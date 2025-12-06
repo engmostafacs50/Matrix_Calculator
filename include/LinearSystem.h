a599d4e9b801810c0b88bf3a347a5792def5e282
Mostafa_Ahmed <most3f3.1112006@gmail.com>
2025-12-06 18:33:44 +0200

    Enhance LinearSystem.h with system explanations
    Added comments explaining non-homogeneous and homogeneous systems.

--- include/LinearSystem.h
+++ include/LinearSystem.h
@@ -1,1 +1,26 @@
 #pragma once
+#include<iostream>
+#include"Matrix.h"
+using namespace std;
+
+/*
+			Non Homogenous System
+1) Rank[A|B]
+
+2)
+If Rank[A|B] != Rank [A] --> No solution
+
+If Rank[A|B] = Rank[A] -> there are two cases in this case 
+
+case one --> number of variables = Rank[A] -> Unique Solution
+case two --> number of variables > Rank[A] -> Infinity Solution
+*/
+
+/*
+			Homogenous System 
+
+1) Rank[A] -> thaere are Two Cases in  this case
+
+case one --> (Rank[A] = number of variables) -> trial solution
+case one --> (Rank[A] < number of variables) -> Infinity Solution
+*/
